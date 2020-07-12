5a00ad25e04569da7c8f7ecf75140f7c2ecc9066
Kevin Ortiz <kevin.ortiz@flatironschool.com>
2020-07-12 15:40:08 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,8 +1,7 @@
 players = ["X","O"]
 def turn_count(board)
   count = 0
-  players.each |player|
-    puts "It is #{player}'s turn."
+  players.each |player|{puts "It is #{player}'s turn."}
     until player == 10
       count += 1
   end
