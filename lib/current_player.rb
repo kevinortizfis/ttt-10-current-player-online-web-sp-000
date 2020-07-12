9abe63338b3e83f8ffa79b510d826d6d6903bce2
Kevin Ortiz <kevin.ortiz@flatironschool.com>
2020-07-12 16:10:44 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -2,7 +2,7 @@ board = [" "," "," "," "," "," "," "," "," "]
 def turn_count(board)
   count = 0
   board.each do |turn|
-    puts "#{turn.to_i}"
+    puts "#{turn}"
   until count == 9
     count += 1
   end
