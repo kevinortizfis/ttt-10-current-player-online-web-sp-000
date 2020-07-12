ace7e1df1286049e1b1df14fbdd731682502ccc9
Kevin Ortiz <kevin.ortiz@flatironschool.com>
2020-07-12 16:00:09 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -2,7 +2,7 @@ board = ["1","2","3","4","5","6","7","8","9"]
 def turn_count(board)
   turn = 0
   board.each do |turn|
-  puts  "It is turn number #{turn}"
+  puts  "#{turn}"
   end
 end
 turn_count(board)
