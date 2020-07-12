745e0dfc244d55ebe7cdbb33b7ab0bf6708b9cac
Kevin Ortiz <kevin.ortiz@flatironschool.com>
2020-07-12 16:13:39 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -9,19 +9,9 @@ def turn_count(board)
   end
 end
 end
-
 turn_count(board)
 
 def current_player(board)
-  count = 0
-  board.each do |turn|
-    puts "#{turn}"
-  until count == 9
-    count += 1
-  end
-end
-end
-turn_count(board)ount(board)
   turn = 0
   until turn >= 9
     players.each do |player|
@@ -31,3 +21,5 @@ turn_count(board)ount(board)
   end
 end
 turn_count(board)
+
+
