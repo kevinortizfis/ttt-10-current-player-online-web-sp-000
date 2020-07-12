3954bd597f01009219e34c501b2936d796a7b2df
Kevin Ortiz <kevin.ortiz@flatironschool.com>
2020-07-12 16:44:07 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -2,7 +2,7 @@ board = [" "," "," "," "," "," "," "," "," "]
 def turn_count(board)
   counter = 0
   board.each do |turn|
-    puts "#{counter}"
+    puts "#{counter + 1}"
     counter +=1
   end
 end
