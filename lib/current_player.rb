c5f5f11da3c5430a5877786b06740be4a5b268a6
Kevin Ortiz <kevin.ortiz@flatironschool.com>
2020-07-12 16:53:18 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -3,7 +3,7 @@ board = [" "," "," "," "," "," "," "," "," "]
 def turn_count(board)
   counter = 1
   board.each do |turn|
-    if turn == "X" || turn == "O"
+    if turn != " "
     puts "#{counter}"
     counter += 1
   end
