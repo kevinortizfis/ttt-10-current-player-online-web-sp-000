9ac248100dfcb02fb359ce70a7782e1c3732835b
Kevin Ortiz <kevin.ortiz@flatironschool.com>
2020-07-12 15:47:11 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,8 +1,11 @@
 players = ["X","O"]
 board = [" "," "," "," "," "," "," "," "," "]
 def turn_count(players)
+  turn = 1
   players.each do |player|
     puts "It is #{player}'s turn."
+    if turn <= 9
+      turn += 1
   end
 end
 turn_count(players)
